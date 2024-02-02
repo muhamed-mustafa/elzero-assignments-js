fc8236cd37961055113ff37aac79752750caba43
Muhammed Mustafa <muuhamed14mustafa@gmail.com>
2024-02-02 17:47:52 +0200

    add some changes

--- js/Introduction.js
+++ js/Introduction.js
@@ -1,49 +1,49 @@
-console.log('Assignment One');
-console.log('----------------');
+console.log("Assignment One");
+console.log("----------------");
 
-let h1 = document.createElement('h1');
-h1.innerHTML = 'Elzero';
-document.body.appendChild(h1);
-
-console.log(
-  '%cElzero %cWeb %cSchool',
-  'color:red',
-  'color:green',
-  'color:blue'
+document.body.appendChild(
+  Object.assign(document.createElement("h1"), { innerHTML: "ElZERO" })
 );
 
-console.group('Group1');
-console.log('Message1');
-console.log('Message2');
-console.group('Child Group');
-console.log('Message1');
-console.log('Message2');
-console.group('Grand Child Group');
-console.log('Message1');
-console.log('Message2');
+const commonStyles = "font-size:40px";
+
+console.log(
+  `%cElZERO %cWEB %cSCHOOL`,
+  `color:red; ${commonStyles}`,
+  `color:green; ${commonStyles}`,
+  `color:blue; ${commonStyles}`
+);
+
+console.group("Group1");
+console.log("Message1");
+console.log("Message2");
+console.group("Child Group");
+console.log("Message1");
+console.log("Message2");
+console.group("Grand Child Group");
+console.log("Message1");
+console.log("Message2");
 console.groupEnd();
 console.groupEnd();
 console.groupEnd();
-console.group('Group2');
-console.log('Message1');
-console.log('Message2');
+console.group("Group2");
+console.log("Message1");
+console.log("Message2");
 console.groupEnd();
 
-console.table(['Mo', 'Mustafa', 'Hamaza']);
+console.table(["Muhammed", "Mustafa", "Al-SAYED"]);
 
 // console.log("Iam In Console"); Single Line Comment
 
 /*
   MultiLine Comment
-  document.write("Iam In Page");
-*/
-
-console.log('-----------------------------');
+   document.write("Iam In Page");
+ */
 
 // Variable And Concatenation Challenge
-let theTitle = 'Elzero',
-  theDescription = 'Elzero Web School',
-  theDate = '25/10';
+let theTitle = "ElZERO",
+  theDescription = "ElZERO Web School",
+  theDate = "25/10";
 
 let pageContent = `<div class = "card" style = "text-align: center;">
   <div class = "child">
